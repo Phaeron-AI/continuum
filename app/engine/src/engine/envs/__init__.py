6a208da73f3e4b9a211d31c2e73bb28848effade
Aatti13 <aattreya.ks@gmail.com>
2026-07-03 21:21:15 +0530

    docs: added init file to envs parent directory to easily access policies and grid2d

---
+++ app/engine/src/engine/envs/__init__.py
@@ -0,0 +1,4 @@
+from .policies import Policy, RandomPolicy
+from .grid2d import Grid2DConfig, Grid2DEnv, Grid2DRenderer, _Grid2DState
+
+__all__ = ["Grid2DConfig", "Grid2DEnv", "Grid2DRenderer", "_Grid2DState"]
